c95ccebc421d8b2d9a91d3b946d502b73b021565
Jean Nunes <jaon@itriad.org.br>
2020-10-21 17:05:02 -0400

    Adding Material

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import React from "react";
 import Routes from "./routes";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import {
+  createMuiTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+
+let theme = createMuiTheme();
+theme = responsiveFontSizes(theme);
 
 function App() {
-  return <Routes />;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Routes />
+    </ThemeProvider>
+  );
 }
 
 export default App;
