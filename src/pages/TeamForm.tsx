f51076df7b6c76f1e693ba105f76b3c10a1d2dc0
Jean Nunes <jaon@itriad.org.br>
2020-10-21 16:29:32 -0400

    Adding routes

---
+++ src/pages/TeamForm.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TeamForm() {
+  return <h1>TeamForm</h1>;
+}
+
+export default TeamForm;
---
+++ src/routes.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Dashboard from "./pages/Dashboard";
+import TeamForm from "./pages/TeamForm";
+
+function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Dashboard} />
+        <Route path="/team" component={TeamForm} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
