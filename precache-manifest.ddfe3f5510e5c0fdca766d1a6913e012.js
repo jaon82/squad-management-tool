f729902616269c8bb1a95c5e907296eb8d697c1d
Jean Nunes <jaon@itriad.org.br>
2020-10-25 22:52:19 -0400

    Updates

---
+++ precache-manifest.ddfe3f5510e5c0fdca766d1a6913e012.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7a3858846fc7896106825ae64a000c1",
+    "url": "/jaon82/squad-management-tool/index.html"
+  },
+  {
+    "revision": "d48be77fc92009e7e832",
+    "url": "/jaon82/squad-management-tool/static/css/main.b6bc4ccb.chunk.css"
+  },
+  {
+    "revision": "05ed481d4fd8e50667ed",
+    "url": "/jaon82/squad-management-tool/static/js/2.4299b139.chunk.js"
+  },
+  {
+    "revision": "966dc248bfab9007e5f5a311088c3b48",
+    "url": "/jaon82/squad-management-tool/static/js/2.4299b139.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68331165260635a5d05b",
+    "url": "/jaon82/squad-management-tool/static/js/3.27f56faf.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/jaon82/squad-management-tool/static/js/3.27f56faf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84ea8f4357bead60ffe0",
+    "url": "/jaon82/squad-management-tool/static/js/4.717278d2.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/jaon82/squad-management-tool/static/js/4.717278d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7abf46cc48636ccc57ea",
+    "url": "/jaon82/squad-management-tool/static/js/5.a232dff1.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/jaon82/squad-management-tool/static/js/5.a232dff1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d48be77fc92009e7e832",
+    "url": "/jaon82/squad-management-tool/static/js/main.bcaa5b92.chunk.js"
+  },
+  {
+    "revision": "61a2c2506e4537151cd2",
+    "url": "/jaon82/squad-management-tool/static/js/runtime-main.76395b97.js"
+  }
+]);
